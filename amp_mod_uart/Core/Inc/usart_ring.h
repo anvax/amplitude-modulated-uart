0cc781ceb764476d8bb8a663ad5f203aadd776fc
anvax <andreykhudobin23@gmail.com>
2024-01-10 20:02:28 +0300

    commit

---
+++ amp_mod_uart/Core/Inc/usart_ring.h
@@ -0,0 +1,20 @@
+/*
+ * usart_ring.h
+ *
+ *  Created on: Dec 26, 2023
+ *      Author: 1
+ */
+
+#ifndef USART_RING_H_
+#define USART_RING_H_
+
+#include "main.h"
+
+#define MYUART huart2 // задефайнить USART
+#define UART_RX_BUFFER_SIZE 128 // указать размер приёмного буфера
+
+uint16_t uart_available(void);
+uint8_t uart_read(void);
+void clear_uart_buff();
+
+#endif
